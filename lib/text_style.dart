6e901e10167c39532c3b21c98438096b250fe42b
Tola-lemma <tolalemma@gmail.com>
2023-01-26 01:33:42 -0800

    add text style class

---
+++ lib/text_style.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class CustomTextStyle {
+  static const TextStyle title = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    color: Color(0xFF2E00FE),
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle mainTitle = TextStyle(
+    fontFamily: 'Poppins',
+    color: Colors.white,
+    fontSize: 22,
+    );
+  static const TextStyle subtitle = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle buttonText = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle subtitleText = TextStyle(
+    fontSize: 12,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+  );
+}
